3277822cd68e49f820e3addf60500dd3a2d3db67
isa <alfred.nikitin@Mail.ru>
2024-08-17 08:47:48 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,21 @@
+# Первый вариант решения
+name = "Alfred"
+print ( name )
+age = 21
+print ( age )
+age = age + 1
+print ( age )
+is_student = True
+print ( is_student )
+
+# Второй вариант решения (более красивее)
+
+name = "Alfred"
+print ( "Name: " , name )
+age = 21
+print ( "Age: " , age )
+age = age + 1
+print ( "New Age: " , age )
+is_student = True
+print ( "Is Student: " , is_student )
+
